f8ce62e293ea332bda25aa06abd09f1adc544d50
ducphatnguyen12042011-prog <ducphatnguyen12042011@gmail.com>
2026-02-22 15:29:00 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,27 +1,26 @@
 import pymongo
 import os
 
-# Kết nối tới MongoDB (Lấy link từ biến môi trường Railway)
+# Kết nối MongoDB từ Railway
 MONGO_URL = os.getenv('MONGO_URL')
 client = pymongo.MongoClient(MONGO_URL)
-db = client['GameDatabase']
+db = client['CentralCashSystem']
 users_col = db['users']
 
 class Economy:
     @staticmethod
-    def get_balance(user_id):
-        user_id = str(user_id) # Luôn lưu ID dưới dạng chuỗi
+    def get_user(user_id):
+        user_id = str(user_id)
         user = users_col.find_one({"user_id": user_id})
         if not user:
-            # Tạo mới nếu chưa có (Tặng 10,000 làm vốn)
             user = {"user_id": user_id, "coins": 10000, "win_amt": 0, "lose_amt": 0}
             users_col.insert_one(user)
-        return user['coins']
+        return user
 
     @staticmethod
     def update_balance(user_id, amount):
         user_id = str(user_id)
-        # $inc là lệnh cộng dồn (số âm sẽ là trừ)
+        # $inc là cộng dồn, số âm sẽ tự trừ
         users_col.update_one(
             {"user_id": user_id},
             {"$inc": {"coins": amount}},
@@ -29,10 +28,10 @@ class Economy:
         )
 
     @staticmethod
-    def update_stats(user_id, coins=0, win=0, lose=0):
+    def update_payout(user_id, payout, win=0, lose=0):
         user_id = str(user_id)
         users_col.update_one(
             {"user_id": user_id},
-            {"$inc": {"coins": coins, "win_amt": win, "lose_amt": lose}},
+            {"$inc": {"coins": payout, "win_amt": win, "lose_amt": lose}},
             upsert=True
         )
